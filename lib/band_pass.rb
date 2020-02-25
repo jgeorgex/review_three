eef259223b1f0818f4ddedb44d97bd1558f9558d
Julian George <jules104@gmail.com>
2020-02-25 12:50:32 +0000

    Update filter method to accept frequencies with more than one integer

--- lib/band_pass.rb
+++ lib/band_pass.rb
@@ -1,11 +1,11 @@
 class BandPass
     def filter(frequency,low_range,high_range)
-        if frequency[0] < low_range
-            frequency = [low_range]
-        elsif frequency[0] > high_range
-            frequency = [high_range]
-        else
-            frequency
-        end
+            frequency.map! {|x| if x < low_range
+                                    x = low_range
+                                elsif x > high_range
+                                    x = high_range
+                                else
+                                    x = x
+                                end }
     end
 end
--- spec/band_pass_spec.rb
+++ spec/band_pass_spec.rb
@@ -14,5 +14,25 @@ describe BandPass do
         band_pass = BandPass.new
         expect(band_pass.filter([2],1,3)). to eq([2])
       end
+      it 'filters signal brings 4 up to 5' do
+        band_pass = BandPass.new
+        expect(band_pass.filter([4],5,6)). to eq([5])
+      end
+      it 'filters signal brings 4 down to 3' do
+        band_pass = BandPass.new
+        expect(band_pass.filter([4],1,3)). to eq([3])
+      end
+      it 'filters signal keeps 2,3 down when low is 1 and high is 4 ' do
+        band_pass = BandPass.new
+        expect(band_pass.filter([2,3],1,4)). to eq([2,3])
+      end
+      it 'filters signal keeps 2,3 down when low is 1 and high is 4 ' do
+        band_pass = BandPass.new
+        expect(band_pass.filter([2,3],1,4)). to eq([2,3])
+      end
+      it 'filters signal changes from 1,3 to 2,3 when high,low is 2,4' do
+        band_pass = BandPass.new
+        expect(band_pass.filter([1,3],2,4)). to eq([2,3])
+      end
     end
 end
