902316e138c51945a910cc4ae7fa758da272f6fe
Julian George <jules104@gmail.com>
2020-02-25 16:00:47 +0000

    filter method returns argument error input soundwave is corrupt if nil value is given

--- lib/band_pass.rb
+++ lib/band_pass.rb
@@ -1,11 +1,22 @@
 class BandPass
     def filter(frequency,low_range,high_range)
-            frequency.map! {|x| if x < low_range
+            new_frequency = frequency.map! {|x| if x < low_range
                                     x = low_range
                                 elsif x > high_range
                                     x = high_range
                                 else
                                     x = x
                                 end }
+            check_valid(new_frequency)
     end
+
+    def check_valid(frequency)
+        frequency.all? {|x| if x.is_a?(Integer) == false
+            return "input soundwave is corrupt"
+    else
+            return frequency
+    end}
+    
+    end
+
 end
--- spec/band_pass_spec.rb
+++ spec/band_pass_spec.rb
@@ -34,5 +34,17 @@ describe BandPass do
         band_pass = BandPass.new
         expect(band_pass.filter([1,3],2,4)). to eq([2,3])
       end
+      it 'filters signal changes from 1,2,3,4 to 1,2,3,3 when high,low is 1,3' do
+        band_pass = BandPass.new
+        expect(band_pass.filter([1,2,3,4],1,3)). to eq([1,2,3,3])
+      end
+      it 'filters signal changes from 10,7,8,5 to 9,7,8,6 when high,low is 6,9' do
+        band_pass = BandPass.new
+        expect(band_pass.filter([10,7,8,5],6,9)). to eq([9,7,8,6])
+      end
+      it 'returns argument error "input soundwave is corrupt" if nil value is given' do
+        band_pass = BandPass.new
+        expect(band_pass.filter(["A"],"A","A")). to eq("input soundwave is corrupt")
+      end
     end
 end
