538348a12ac95482799f067e71c3b59a6f711bc7
Julian George <jules104@gmail.com>
2020-02-25 19:06:05 +0000

    Refactor all except filter method tbc

--- lib/band_pass.rb
+++ lib/band_pass.rb
@@ -1,26 +1,25 @@
 class BandPass
     def filter(frequency, low_range, high_range)
-        # p check_low_range(low_range)
         if check_low_range(low_range) == false
             "input soundwave is corrupt"
         elsif check_high_range(high_range) == false
             "input soundwave is corrupt"
-        elsif check_frequency(frequency) == "input soundwave is corrupt"
+        elsif check_frequency(frequency) == false
             return "input soundwave is corrupt"
         else
-          new_frequency = get_new_frequency(frequency, low_range, high_range)
-          check_valid(new_frequency)
+          get_new_frequency(frequency, low_range, high_range)
         end
     end
 
     def get_new_frequency(frequency, low_range, high_range)
-        frequency.map! {|x| if x < low_range
-                                    x = low_range
-                                elsif x > high_range
-                                    x = high_range
-                                else
-                                    x = x
-                                end }
+        frequency.map! {|checked_frequency| if checked_frequency < low_range
+                                                low_range
+                                            elsif checked_frequency > high_range
+                                                high_range
+                                            else
+                                                checked_frequency
+                                            end 
+                        }
     end
 
     def check_valid(frequency)
@@ -37,12 +36,10 @@ class BandPass
 
     def check_frequency(frequency)
         frequency.all? {|x| if x.is_a?(Integer) == false
-            return "input soundwave is corrupt"
-    else
-            return frequency
-    end}
+                               return false
+                            else
+                                return frequency
+                            end
+                        }
     end
-
-
-
 end
